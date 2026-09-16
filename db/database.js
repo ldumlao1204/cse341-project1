d36ff49939bf373357ed91272b39d59559138a3b
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-09-16 22:33:57 +0800

    fixed error in deployment

--- db/database.js
+++ db/database.js
@@ -10,7 +10,7 @@ const initDb = (callback) => {
         console.log('Database is already initialized!');
         return callback(null, database);
     }
-    MongoClient.connect(process.env.MONGODB_URI)
+    MongoClient.connect(process.env.MONGODB_URL)
         .then((client) => {
             database = client.db('cse341-project1');
             callback(null, database);
