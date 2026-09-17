a92de0d2a4ffd8a9a366343cfdc9d043e239e9bc
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-09-17 21:21:19 +0800

    updated port 3000

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Contacts API',
         description: 'Contacts API'
     },
-    host: 'localhost:3001',
+    host: 'localhost:3000',
     schemes: ['https', 'http']
 };
 
