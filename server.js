3593fdb7c0c6fc0004dcf82e0773bb4cb5396e27
Leilany Dumlao <leilany.dumlao@gmail.com>
2026-09-17 21:22:30 +0800

    uipdated server port 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/database');
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
